da372e6ccdf178242c89aaed57cde9752d4226e8
Hammad Ali Khan <hammad.web3.0developer@gmail.com>
2026-02-07 17:45:09 +0500

    03: database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 
 export default function Home() {
+  const createProject = useMutation(api.projects.create)
+  const projects = useQuery(api.projects.get)
+
   return (
     <div>
-      <Button>
-        Hello Hammad
+      <Button
+       onClick={()=>(createProject({name:"Project123"}))}
+       >
+        Add new 
       </Button>
+      {
+        projects?.map((project)=>(
+                <div className="border rounded p-2 flex flex-col" key={project._id}>
+               {project._id}
+               <p>{project.name}</p>
+               <p>Owner Id:{project.ownerId}</p>
+                </div>  
+        )
+      )
+      }
     </div>
   );
 }
