02930ad9d649fb88c7c76db0dd078c01655a2cac
Hammad Ali Khan <hammad.web3.0developer@gmail.com>
2026-03-03 23:37:06 +0500

    14: file breadcrumb

--- src/features/editor/components/top-navigation.tsx
+++ src/features/editor/components/top-navigation.tsx
@@ -1,7 +1,11 @@
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Id } from "../../../../convex/_generated/dataModel";
-import { useEditor } from "../hooks/use-editor";
+import { useEditor} from "../hooks/use-editor";
 import { useFile } from "@/features/projects/hooks/use-files";
+import { cn } from "@/lib/utils";
+import { Spinner } from "@/components/ui/spinner";
+import { FileIcon } from "@react-symbols/icons/utils";
+import { XIcon } from "lucide-react";
 
 
 const Tab = (
@@ -17,9 +21,53 @@ const Tab = (
     }
 )=>{
     const file = useFile(fileId)
+    const {
+        activeTabId,
+        previewTabId,
+        setActiveTab,
+        openFile,
+        closeTab
+    } = useEditor(projectId)
+
+    const isActive = activeTabId === fileId
+    const isPreiew = previewTabId === fileId
+    const fileName = file?.name ?? "Loading..."
+
 return(
-    <div>
-        
+    <div
+    onClick={()=>setActiveTab(fileId)}
+    onDoubleClick={()=>openFile(fileId,{pinned:true})}
+    className={cn("flex items-center gap-2 h-8.75 pl-2 pr-1.5 cursor-pointer text-muted-foreground group border-y border-x border-transparent hover:bg-accent/30",
+        isActive && "bg-background text-foreground border-x-border border-b-background -mb-px drop-shadow",
+        isFirst && "border-l-transparent!"
+    )}
+    >
+        { file === undefined ? (
+                <Spinner className="text-ring"/> 
+            ):(
+                <FileIcon fileName={fileName} autoAssign className="size-4"/>
+            )
+        }
+        <span
+        className={cn("text-sm whitespace-nowrap",
+            isPreiew && "italic"
+        )}
+        >
+            {fileName}
+        </span>
+        <button
+        onClick={(e)=>{
+            e.preventDefault()
+            e.stopPropagation()
+            closeTab(fileId)
+        }}
+        className={cn(
+            "p-0.5 rounded-sm hover:bg-white/10 opacity-0 group-hover:opacity-100",
+            isActive && "opacity-100"
+        )}
+        >
+            <XIcon className="size-3.5"/>
+        </button>
     </div>
 )
 }
